9c6f024982f01382c06a8e57a80be4f4ca6fc6e3
Pratiksha9240 <68505197+Pratiksha9240@users.noreply.github.com>
2022-12-14 19:11:54 +0530

    Converted posts code to async await

---
+++ asyncAwait2.js
@@ -0,0 +1,101 @@
+const posts = [
+    {title: 'Post 1', body: 'This is post one',createdAt: new Date().getTime()},
+    {title: 'Post 2', body: 'This is post Two',createdAt: new Date().getTime()}
+]
+
+let intervalId = 0;
+
+
+
+let post1 = async() => {
+
+    const getPosts = new Promise((resolve,reject) => {
+        clearInterval(intervalId);
+        setInterval(() => {
+                let output = '';
+                posts.forEach((post,index) => {
+                    output = output + `<li>${post.title} created ${(new Date().getTime() - post.createdAt)/1000} seconds ago</li>`;
+                })
+                document.body.innerHTML = output;
+        },1000);
+    })
+
+
+    function createPost(post){
+
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+                posts.push({...post, createdAt: new Date().getTime()});
+                const error = false;
+    
+                if(!error){
+                    resolve();
+                }
+                else{
+                    reject('Error: Something went wrong');
+                }
+    
+            },1000)
+        })
+    }
+    
+    function create4Post(post,callback){
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+                posts.push({...post, createdAt: new Date().getTime()});
+                const error = false;
+    
+                if(!error){
+                    resolve();
+                }
+                else{
+                    reject('Error: Something went wrong');
+                }
+    
+            },1000)
+        })
+    }
+    
+    function deletePost(){
+        return new Promise((resolve,reject) => {
+            let count = posts.length;
+            posts.pop();
+            setTimeout(() => {
+                if(count>0){
+                    resolve();
+                }
+                else{
+                    reject('Array is Empty');
+                }
+            },2000)
+        })
+    }
+    
+    const updateLastUserActivityTime = new Promise((resolve,reject) => {
+        setInterval(() => {
+            posts.forEach((post,index) => {
+                console.log(post.title,((new Date().getTime() - post.createdAt)/1000));
+            })
+            const error = false;
+            if(!error){
+                resolve();
+            }
+            else{
+                reject();
+            }
+    },1000);
+    })
+    
+    await getPosts;
+
+
+    
+    await Promise.all([createPost({title: 'Post 3', body: 'This is post Three'}),create4Post({title: 'Post 4', body: 'This is post four'}),updateLastUserActivityTime]);
+}
+
+post1().then(() => {
+    getPosts(),
+    setTimeout(() => {
+        deletePost().then(() => {getPosts()})
+    },5000)
+});
